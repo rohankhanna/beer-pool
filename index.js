446bc9e49de850258594b2016a2bd9a52e97b463
Rohan Khanna <rohan@kanerika.com>
2017-02-21 19:22:36 +0530

    closer to achieving desired functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redisClient = require('redis').createClient();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
