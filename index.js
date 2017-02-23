ba28723219dff8144c7f930d3b5c1d366b507486
Rohan Khanna <rohan@kanerika.com>
2017-02-23 11:58:31 +0530

    restructured code

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var redisClient = require('redis').createClient();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
 
 io.on('connection', function(socket){
+  console.log('socket connected');
   socket.on('chat message', function(msg){
     console.log(msg);
     io.emit('chat message', msg);
   });
+  socket.on('nickname', function(nickname){
+    console.log(msg);
+    io.broadcast('nickname', { nickname:nickname, joined: true });
+  });
 });
+
+io.on('disconnect', function(socket){
+  console.log('disconnected');
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('nickname', function(nickname){
+    console.log(msg);
+    io.broadcast('nickname', { nickname:nickname, joined: true });
+  });
+});
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
